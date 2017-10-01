36ac10af63634ed84e91de2f2570a3840484d583
katnes <kapo123113@gmail.com>
2017-10-01 14:39:39 -0500

    Fixed spacing issues and added veritical lines

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("This file is linked!");
+var teacherName = "Ignis Scientia";
+var department = "Physics";
+var rating = ["4.5", "4.8", "5.0"];
+
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + rating);
+
+var avg = (2.3+1.2+.5) / (3);
+var aveRating = Math.round(avg * 100) / 100;
+console.log("Avg Rating: " + aveRating);
