45f39788c863408162ca294a326a074d70866009
katnes <kapo123113@gmail.com>
2017-12-05 19:14:41 -0600

    Pushing for Desktop on 12/5

--- js/main.js
+++ js/main.js
@@ -10,3 +10,42 @@ console.log("Ratings: " + rating);
 var avg = (2.3+1.2+.5) / (3);
 var aveRating = Math.round(avg * 100) / 100;
 console.log("Avg Rating: " + aveRating);
+
+console.log("This file is linked!");
+var teacherName = "Gladiolus Amicitia";
+var department = "Physical Education";
+var rating = ["3.5", "4.9", "5.0"];
+
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + rating);
+
+var avg = (2.3+1.2+.5) / (3);
+var aveRating = Math.round(avg * 100) / 100;
+console.log("Avg Rating: " + aveRating);
+
+console.log("This file is linked!");
+var teacherName = "Ardyn Izunia";
+var department = "Psychology";
+var rating = ["3.5", "4.9", "5.0"];
+
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + rating);
+
+var avg = (2.3+1.2+.5) / (3);
+var aveRating = Math.round(avg * 100) / 100;
+console.log("Avg Rating: " + aveRating);
+
+console.log("This file is linked!");
+var teacherName = " Gentiana ";
+var department = "Psychology";
+var rating = ["5", "4.9", "5.0"];
+
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + rating);
+
+var avg = (2.3+1.2+.5) / (3);
+var aveRating = Math.round(avg * 100) / 100;
+console.log("Avg Rating: " + aveRating);
