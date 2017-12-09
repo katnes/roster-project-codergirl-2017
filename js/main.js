9c3d557f22cf14aa9780140a1bab6f0179b220f5
katnes <kapo123113@gmail.com>
2017-12-09 11:25:52 -0600

    Add'l teachers and JS structure

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
+
+
 console.log("This file is linked!");
+/* File Link test */
+alert("Welcome to the FFXV Teacher Page!")
+/* JS Testing */
+
+
+/*Ignis*/
+
+
 var teacherName = "Ignis Scientia";
-var department = "Physics";
-var rating = ["4.5", "4.8", "5.0"];
+var department = "Culinary Arts";
+var rating = [5.0, 5.0, 4.9];
 
 console.log("Teacher: " + teacherName);
-console.log("Department: " + department);
+console.log("Department: " +  department);
 console.log("Ratings: " + rating);
 
-var avg = (2.3+1.2+.5) / (3);
-var aveRating = Math.round(avg * 100) / 100;
-console.log("Avg Rating: " + aveRating);
+function aveRating (rating) {
 
-console.log("This file is linked!");
+  var result = rating / 3; /*divide ratings by 3 to find the average*/
+  var avg = (result *100) /100; /*round the result */
+
+   return ave;
+}
+
+console.log("Average Rating:" + avgRating());
+
+
+/*Glady*/
+
 var teacherName = "Gladiolus Amicitia";
 var department = "Physical Education";
 var rating = ["3.5", "4.9", "5.0"];
